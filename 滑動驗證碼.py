f04f310621232080173d9a66a4b0539aa9603f3b
kklefty <kklefty@gmail.com>
2019-11-19 16:47:29 +0800

    滑動驗證，修正目標判定方式

--- 滑動驗證碼.py
+++ 滑動驗證碼.py
@@ -1,5 +1,7 @@
 #借鑑來源 簡書:https://www.jianshu.com/p/25a42d97185b
 
+#修正精準定位，
+
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -15,7 +17,7 @@ class CrackSlider():
 
     #使用滑動驗證網站
     def __init__(self):
-        self.url = 'https://test.testURLcom/'
+        self.url = "https://member.igoldhk.com" # 'https://xxx.test.com'
         self.driver = webdriver.Chrome()
         self.wait = WebDriverWait(self.driver, 20)
         self.zoom = 1
@@ -32,8 +34,9 @@ class CrackSlider():
         
     #獲得驗證圖片與小拼圖
     def get_pic(self):
-        mouse = self.driver.find_element_by_class_name('slideCode_slider') #使用滑鼠懸停觸發驗證圖片顯示
-        ActionChains(self.driver).move_to_element(mouse).perform()
+        # mouse = self.driver.find_element_by_class_name('slideCode_slider') #使用滑鼠懸停觸發驗證圖片顯示
+        # ActionChains(self.driver).move_to_element(mouse).perform()
+        self.driver.find_element_by_class_name('slideCode_slider').click()
         time.sleep(1)
         target = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'slideCode_bg-img'))) # 獲得有缺口的圖片
         template = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'slideCode_jigsaw'))) # 獲得小拼圖
@@ -46,9 +49,7 @@ class CrackSlider():
         template_img.save('template.png')
         local_img = cv2.imread('target.jpg') # 使用opencv讀取缺口圖片
         size_loc = local_img.shape # 獲得缺口圖片大小(高、寬、頻道(彩色))
-        print(size_loc)
-        self.zoom = int(size_loc[0])/500 # 獲得缺口圖片寬度除以500數值當作結果值，作為縮放比例(拼圖移動會使用)
-
+        self.zoom = 400/int(size_loc[1]) # 網頁框架與實際圖片比例(拼圖移動會使用)，此處前端顯示寬度為400
     def get_tracks(self, distance):
         print('distance : ',distance) # 顯示移動目的 X 座標
         distance += 10 # 模擬人為操作，拼圖移動超過缺口位置的距離
@@ -57,7 +58,7 @@ class CrackSlider():
         forward_tracks = []
         current = 0
         mid = distance * 3 / 5  # 移動壘加的臨界值(下方判斷超過臨界值後執行遞減)，模擬人為操作拼圖快到缺口減速之動作
-        while current < distance:
+        while True:
             if current < mid:
                 a = 2  #加速度 2
             else:
@@ -65,50 +66,66 @@ class CrackSlider():
             s = v * t + 0.5 * a * (t ** 2)
             v = v + a * t
             current += s
-            forward_tracks.append(round(s))
+            if current < distance:
+                forward_tracks.append(round(s))
+            else:
+                forward_tracks.append(round(distance+s-current)) # 避免總移動長度超過目標缺口，最後一個移動距離為剩下的 X 距離
+                break
         print(forward_tracks)
-        back_tracks = [-3, -3, -2, -2, -2, -2, -2, -1, -1, -1]
+        back_tracks = [-3, -2, -2, -2, -1] #超過缺口後拼圖往左移動
         return {'forward_tracks': forward_tracks, 'back_tracks': back_tracks}
 
     def match(self, target, template):
         img_rgb = cv2.imread(target) # 載入有缺口的圖片
         img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) # 進行灰度化處理
         template = cv2.imread(template, 0) # 載入要搜索的圖片(可以滑動那張小拼圖)
-        w, h = template.shape[::-1] # shape會顯示，高、寬與頻道(彩色3,灰諧1)，此處將最後一個數值del，因此紀錄小拼圖的圖像尺寸 W:寬 H:高
+        w, h = template.shape[::-1] # shape會顯示，寬、高與頻道(彩色3,灰諧1)，此處將最後一個數值del，因此紀錄小拼圖的圖像尺寸 W:寬 H:高
         print(w, h)
 
         # 開始進行匹配
         res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-        # 得到兩組array，第一組因選擇TM_CCOEFF_NORMED，輸出值會式-1~1之間，第二組將會獲得第一組相對應的X座標位置
-        # 判斷正確的位置便是當array[1]只剩下一個數值，及判斷圖片 X 座標
-        run = 1
+        # res 會得到array，因選擇TM_CCOEFF_NORMED，輸出值會式-1~1之間，越靠近-1則代表較不符合，反之靠近1則代表越符合
 
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res) # 找尋array中最大值與最小值，以及相對應的位置
+        print("最大值",max_loc,max_val)
+
+        # 另外顯示匹配結果
+        bottom_right = (max_loc[0] + w, h)
+        img = cv2.rectangle(img_rgb, max_loc, bottom_right, (0, 255), 2)  # 劃出一個長方形(目標圖片,起始座標,相對座標,RGB顏色,線條寬度 )
+        print("目標座標範圍 :\nx:%r - %r\ny:%r - %r" % (max_loc[0], bottom_right[0], max_loc[1], bottom_right[1]))
+        cv2.imwrite('target_OK.jpg', img)
+        cv2.imshow('Find_target', img) # 彈出視窗並顯示標示畫線區域結果
+        cv2.waitKey(0) # 等待任何按鍵觸發
+        cv2.destroyAllWindows() # 關閉顯示畫線圖片(target_OK)
+
+        return max_loc[0]
+
+        # run = 1
         # 使用二分法查找阈值的精确值
-        L = 0
-        R = 1
-        while run < 20:
-            run += 1
-            threshold = (R + L) / 2
-            print(threshold)
-            if threshold < 0:
-                print('Error')
-                return None
-            loc = np.where(res >= threshold)
-            #print(loc)
-            print(len(loc[1]))
-            if len(loc[1]) > 1:
-                L += (R - L) / 2
-            elif len(loc[1]) == 1:
-                print('缺口起始座標X位置：%d' % loc[1][0])
-                break
-            elif len(loc[1]) < 1:
-                R -= (R - L) / 2
-        return loc[1][0]
+        # L = 0
+        # R = 1
+        # while run < 20:
+        #     run += 1
+        #     threshold = (R + L) / 2
+        #     print(threshold)
+        #     if threshold < 0:
+        #         print('Error')
+        #         return None
+        #     loc = np.where(res >= threshold)
+        #     if len(loc[1]) > 1:
+        #         L += (R - L) / 2
+        #     elif len(loc[1]) == 1:
+        #         print('缺口起始座標X位置：%d' % loc[1][0])
+        #         break
+        #     elif len(loc[1]) < 1:
+        #         R -= (R - L) / 2
+
+
 
     # 移動拼圖
     def crack_slider(self,tracks):
         slider = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'slideCode_slider')))
-        ActionChains(self.driver).click_and_hold(slider).perform()
+        ActionChains(self.driver).click_and_hold(slider).perform() #滑鼠左鍵持續按著不放
 
         # 往右移動，使用forward_tracks列表當作移動軌跡
         for track in tracks['forward_tracks']:
@@ -125,7 +142,7 @@ class CrackSlider():
         ActionChains(self.driver).move_by_offset(xoffset=4, yoffset=0).perform()
         time.sleep(0.5)
 
-        ActionChains(self.driver).release().perform()
+        ActionChains(self.driver).release().perform() # 釋放滑鼠左鍵
 
     def login(self):
         self.driver.find_element_by_id('userLogin').click()
@@ -139,7 +156,7 @@ if __name__ == '__main__':
         template = 'template.png'
         cs.get_pic()
         distance = cs.match(target, template)
-        tracks = cs.get_tracks((distance + 7) * cs.zoom)  # 对位移的缩放计算
+        tracks = cs.get_tracks((distance)* cs.zoom) # 移動距離乘以"網頁"與"實際圖片"寬度的比值。
         cs.crack_slider(tracks)
         print('移動完成')
         time.sleep(1)
